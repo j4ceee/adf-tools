9fabedeae9089a28b1ea10981a16281c6cce5bf3
Cedric <130901176+j4ceee@users.noreply.github.com>
2025-05-09 19:54:01 +0200

    Refactor emoji exporting to support direct unicode output
    - emojis with this format ":text:" get filtered
    - display emojis directly

--- src/Exporter/Html/Inline/EmojiExporter.php
+++ src/Exporter/Html/Inline/EmojiExporter.php
@@ -13,10 +13,11 @@ class EmojiExporter extends HtmlExporter
     {
         \assert($this->node instanceof Emoji);
 
-        return sprintf(
-            '<img class="adf-emoji" loading="lazy" src="https://pf-emoji-service--cdn.us-east-1.prod.public.atl-paas.net/standard/a51a7674-8d5d-4495-a2d2-a67c090f5c3b/64x64/%s.png" alt=":%s:" width="20" height="20">',
-            $this->node->getId(),
-            $this->node->getShortName()
-        );
+        $text = $this->node->getText();
+        if (preg_match(":[a-zA-Z]+:", $text)) {
+            return "";
+        } else {
+            return $this->node->getText();
+        }
     }
 }
